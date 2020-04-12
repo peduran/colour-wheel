4a8bc4a2e11c6554f8d81402d29f8402b7afa8bf
Aiden Scott <aidenscott12@googlemail.com>
2020-04-12 21:29:21 +0100

    enhance

--- src/picker.tsx
+++ src/picker.tsx
@@ -1,25 +1,38 @@
 import React from "react";
 export default () => {
-  const ref = React.useRef<HTMLCanvasElement>(null);
+  const ref1 = React.useRef<HTMLCanvasElement>(null);
+  const ref2 = React.useRef<HTMLCanvasElement>(null);
 
   const getRandomInt = (max: number): number =>
     Math.floor(Math.random() * Math.floor(max));
   React.useEffect(() => {
-    const canvas = ref.current;
-    const ctx = canvas!.getContext("2d")!;
+    const canvas1 = ref1.current;
+    const canvas2 = ref2.current;
+    const ctx = canvas1!.getContext("2d")!;
+    const zoomCtx = canvas2!.getContext("2d")
+    zoomCtx!.imageSmoothingEnabled = false
+
     const imageData = ctx.createImageData(100, 100);
     imageData.data.set(imageData.data.map(() => getRandomInt(255)));
+    ctx.putImageData(imageData, 0,0);
 
-    ctx.putImageData(imageData, 100, 100);
+    zoomCtx?.drawImage(canvas1!, 0,0, 1000,1000)
+
   }, []);
 
   return (
     <>
       <canvas
-        ref={ref}
+        ref={ref1}
         className="color-canvas"
-        width={window.innerWidth}
-        height={window.innerHeight}
+        width={100}
+        height={100}
+      ></canvas>
+      <canvas
+        ref={ref2}
+        className="color-canvas-2"
+        width={1000}
+        height={1000}
       ></canvas>
     </>
   );
