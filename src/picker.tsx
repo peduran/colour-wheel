1c54e3024287732c2934d1484c2cca4915f30d52
Aiden Scott <aidenscott12@googlemail.com>
2020-04-18 15:15:58 +0100

    wip oscillator

--- src/picker.tsx
+++ src/picker.tsx
@@ -1,24 +1,39 @@
 import React from "react"
 import { Option, None, Some } from "space-lift"
+import useOscillator from "./useOscillatorHook"
+import { inflateRaw } from "zlib"
+
+const getRandomInt = (max: number): number =>
+  Math.floor(Math.random() * Math.floor(max))
 
 export default () => {
+  console.log("rendered")
   const smallCanvasRef = React.useRef<HTMLCanvasElement>(null)
   const zoomCanvasRef = React.useRef<HTMLCanvasElement>(null)
   const [url, setUrl] = React.useState<Option<string>>(None)
   const [colour, setColour] = React.useState<string>("white")
+  const [oscillator] = useOscillator()
 
-  const getRandomInt = (max: number): number =>
-    Math.floor(Math.random() * Math.floor(max))
-
-  const setColourOnMouseMove = (context: CanvasRenderingContext2D) => (
-    e: MouseEvent
+  const handleMouseMove  = (
+    e: React.MouseEvent
   ): void => {
     const [x, y] = [e.clientX, e.clientY]
-    const [r, g, b, a] = (context.getImageData(x, y, 1, 1)
+    const [r, g, b, a] = (zoomCanvasRef.current?.getContext('2d')!.getImageData(x, y, 1, 1)
       .data as unknown) as Array<number>
-    const rgba = `rgba(${r}, ${g}, ${b}, ${a})`
-    console.log(rgba)
-    return setColour(rgba)
+
+    const setColourOnMouseMove = () => {
+      const rgba = `rgba(${r}, ${g}, ${b}, ${a})`
+      setColour(rgba)
+    }
+
+    const setOscilltorOnMouseMove = () => {
+      console.log(r + g + b + a)
+      oscillator.frequency.value = r +g +b +a 
+    
+    }
+
+    setColourOnMouseMove()
+    setOscilltorOnMouseMove()
   }
 
   React.useEffect(() => {
@@ -27,7 +42,7 @@ export default () => {
       ([smallCanvas, zoomCanvas]) => {
         Option.all([
           smallCanvas.getContext("2d"),
-          zoomCanvas.getContext("2d")
+          zoomCanvas.getContext("2d"),
         ]).fold(
           () => console.error("get 2d context fails"),
           ([smallCanvasContext, zoomCanvasContext]) => {
@@ -38,13 +53,6 @@ export default () => {
 
             smallCanvasContext.putImageData(imageData, 0, 0)
             zoomCanvasContext.drawImage(smallCanvas, 0, 0, 1000, 1000)
-
-            setUrl(Some(zoomCanvas.toDataURL()))
-
-            zoomCanvas.addEventListener(
-              "mousemove",
-              setColourOnMouseMove(zoomCanvasContext)
-            )
           }
         )
       }
@@ -59,19 +67,38 @@ export default () => {
           className="small-canvas"
           width={100}
           height={100}
+          style={{display:'none'}}
         ></canvas>
         <canvas
           ref={zoomCanvasRef}
           className="zoom-canvas"
           width={1000}
           height={1000}
+          onMouseMove={handleMouseMove}
         ></canvas>
-        {url.map(_ => (
+        <OscilatorControl oscillator={oscillator}/>
+        {url.map((_) => (
           <a key="1" href={_} download="image.png">
             download
           </a>
         ))}
       </div>
+    </>
+  )
+}
+
+interface OscilatorControlProps {
+  oscillator: OscillatorNode
+}
+const OscilatorControl = ({oscillator}: OscilatorControlProps) => {
+  return (
+    <>
+      <button onClick={() => oscillator.stop()}>stop</button>
+      <button onClick={() => oscillator.start()}>start</button>
+      <input
+        value={oscillator.frequency.value}
+        onChange={(e) => { oscillator.frequency.value = Number(e.target.value)}}
+      />
     </>
   )
 }
